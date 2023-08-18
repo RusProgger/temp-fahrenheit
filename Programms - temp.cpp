d733d315ca79f965bff96526b203a8f106f2e827
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2023-08-18 23:37:35 +0300

    Add files via upload

---
+++ Programms - temp.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void temp() {
+	double temp; // создаем переменную для ввода температуры фаренгейт
+	std::cout << "Введите температуру в фаренгейт: ";
+	std::cin >> temp;
+
+
+	// Алгоритм решения в перевод в Цельсий
+
+	int result_temp = (temp - 32) * 5 / 9;
+
+	std::cout << "Температура в фаренгейт: " << temp << endl;
+	Sleep(1500);
+	std::cout << "Температура в цельсиях: " << result_temp << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	temp();
+
+
+	system("pause");
+	return 0;
+}
